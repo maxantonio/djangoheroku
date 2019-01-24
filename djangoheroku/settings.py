7afde1b831f84a6591a6fc2bef7c6fb3fc203651
maxantonio <mycybornet@gmail.com>
2019-01-24 16:00:20 -0500

    + django heroku settings

--- djangoheroku/settings.py
+++ djangoheroku/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+# # Configure Django App for Heroku.
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,6 +128,5 @@ STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, 'static'),
 ]
 
-
-# # Configure Django App for Heroku.
-import django_heroku
+# setting in heroku
+django_heroku.settings(locals())
